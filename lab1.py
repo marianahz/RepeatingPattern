7079f5005f616cf31d7c9b0ba8273b588235d0c5
Mariana <57423007+marianahz@users.noreply.github.com>
2024-05-18 12:05:23 -0500

    Add files via upload

--- lab1.py
+++ lab1.py
@@ -8,18 +8,18 @@ def repeats(list):
     longest = None
     # See if the pattern repeats
 
-    def isRepeating(pattern):
-        patternLength = len(pattern)
-        for i in range(patternLength, n):
-            if list[i] != pattern[i % patternLength]:
+    def is_repeating(pattern):
+        pattern_length = len(pattern)
+        for i in range(pattern_length, n):
+            if list[i] != pattern[i % pattern_length]:
                 return False
         return True
     
-    for patternLength in range(1, n // 2 + 1):
+    for pattern_length in range(1, n // 2 + 1):
         # Can the list be divided into patterns that are equal
-        if n % patternLength == 0:
-            pattern = list[: patternLength]
-            if isRepeating(pattern) and (longest is None or patternLength > len(longest)):
+        if n % pattern_length == 0:
+            pattern = list[: pattern_length]
+            if is_repeating(pattern) and (longest is None or pattern_length > len(longest)):
                 longest = pattern
                 return pattern
     return longest
